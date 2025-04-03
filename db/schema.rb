1247d5f2536efbea2477f4c84f81f0d8305c86d8
GlennIvander <ivander792@gmail.com>
2025-04-04 00:40:50 +0800

    first commit. Ends with comments index

---
+++ db/schema.rb
@@ -0,0 +1,23 @@
+ActiveRecord::Schema[8.0].define(version: 2025_04_02_112235) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "is_published", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
+end
